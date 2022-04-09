f7de1dee0b5ee4091570e8c6efff7873d240a250
Johan Jacobs <johanjacobs.sa@gmail.com>
2022-04-10 07:29:31 +1200

    Log: Save log data after each trace.

--- FrecklesEngine/src/FE/Core/Log/Log.cpp
+++ FrecklesEngine/src/FE/Core/Log/Log.cpp
@@ -24,7 +24,9 @@ namespace FE
 
             //enable the logs
             CoreLogger->set_level(spdlog::level::trace);
+            CoreLogger->flush_on(spdlog::level::trace);
             ClientLogger->set_level(spdlog::level::trace);
+            
            
         }
         Log::~Log()
