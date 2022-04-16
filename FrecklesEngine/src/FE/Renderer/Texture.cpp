e68f8ba3ea1b1a3f0b8ef67e1303f971844bc555
JohanJacobs <johanjacobs.sa@gmail.com>
2022-04-16 23:16:33 +1200

    extend textures to create blank textures for frame buffer

--- FrecklesEngine/src/FE/Renderer/Texture.cpp
+++ FrecklesEngine/src/FE/Renderer/Texture.cpp
@@ -18,6 +18,7 @@ namespace FE
 			stbi_set_flip_vertically_on_load(1); // flip image
 			unsigned char* data = stbi_load(filePath.c_str(), &width, &height, &nrChannels,0);
 			
+			uint32_t ImageFormat = GL_RGB;
 			if (data)
 			{
 				LOG_CORE_INFO("Loaded Texture '{}'", filePath.c_str());
@@ -39,9 +40,12 @@ namespace FE
 				LOG_CORE_ERROR("Invalid image Format, Channels: {}", nrChannels);
 				}
 
+				Width = width;
+				Height = height;
 				// texture 
 				glCreateTextures(GL_TEXTURE_2D, 1, &RenderID);
 				glTextureStorage2D(RenderID, 1, InternalFormat, width, height);
+
 				// set the texture wrapping/filtering options (on the currently bound texture object)
 				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
@@ -56,6 +60,23 @@ namespace FE
 				LOG_ERROR("file not loaded");
 			}
 			stbi_image_free(data);
+		}
+
+		Texture2D::Texture2D(uint32_t width, uint32_t height)
+		{
+			Width = width;
+			Height = height;
+			
+			InternalFormat = GL_RGBA8;
+			// texture 
+			glCreateTextures(GL_TEXTURE_2D, 1, &RenderID);			
+			glTextureStorage2D(RenderID, 1, InternalFormat, width, height);			
+
+			// set the texture wrapping/filtering options (on the currently bound texture object)
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);						
 		}
 
 		Texture2D::~Texture2D()
@@ -75,5 +96,12 @@ namespace FE
 
 			return CreateRef<Texture2D>(filePath);
 		}
+
+		Ref<FE::RENDERER::Texture2D> Texture2D::Create(uint32_t width, uint32_t height)
+		{
+			LOG_CORE_TRACE(LOG_FUNCTION_NAME);
+			return CreateRef<Texture2D>(width, height);
+		}
+
 	}
 }
--- FrecklesEngine/src/FE/Renderer/Texture.hpp
+++ FrecklesEngine/src/FE/Renderer/Texture.hpp
@@ -16,14 +16,20 @@ namespace FE
 		class Texture2D:public Texture
 		{
 		public:
-			Texture2D(const std::string& filePath);
+			explicit Texture2D(const std::string& filePath);
+			explicit Texture2D(uint32_t width, uint32_t height);
+
 			~Texture2D();
 			void Bind(uint32_t slot = 0) const;
 
+			uint32_t GetRenderID() const { return RenderID; }
 			static Ref<Texture2D> Create(const std::string& filePath);
+			static Ref<Texture2D> Create(uint32_t width, uint32_t height) ;
+
 		private:
 			uint32_t RenderID;
-			uint32_t InternalFormat{ 0 }, ImageFormat{0};
+			uint32_t InternalFormat{ 0 };
+			uint32_t Width{0}, Height{ 0 };
 		};
 
 	}
