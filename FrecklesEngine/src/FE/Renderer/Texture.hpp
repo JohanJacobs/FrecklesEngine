2a61b77e92f2f01ec25a240383374bd2c08eae23
JohanJacobs <johanjacobs.sa@gmail.com>
2022-04-18 15:15:44 +1200

    extend textures to create blank textures for framebuffer

--- FrecklesEngine/src/FE/Renderer/Texture.hpp
+++ FrecklesEngine/src/FE/Renderer/Texture.hpp
@@ -16,15 +16,21 @@ namespace FE
 		class Texture2D:public Texture
 		{
 		public:
-			Texture2D(const std::string& filePath);
+			explicit Texture2D(const std::string& filePath);
+			explicit Texture2D(uint32_t width, uint32_t height);
+
 			~Texture2D();
 			void Bind(uint32_t slot = 0) const;
 			void Unbind() const;
 
+			uint32_t GetRenderID() const { return RenderID; }
 			static Ref<Texture2D> Create(const std::string& filePath);
+			static Ref<Texture2D> Create(uint32_t width, uint32_t height) ;
+
 		private:
 			uint32_t RenderID;
-			uint32_t InternalFormat{ 0 }, ImageFormat{0};
+			uint32_t InternalFormat{ 0 };
+			uint32_t Width{0}, Height{ 0 };
 		};
 
 	}
