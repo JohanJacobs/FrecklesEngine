025d1d9c1b19f1987dbd15ac1ee19c1aff675555
JohanJacobs <johanjacobs.sa@gmail.com>
2022-04-25 20:46:07 +1200

    3D sample scene with basic mouse movement.

--- TestApp3D/src/TestAppLayer/TestAppLayer.cpp
+++ TestApp3D/src/TestAppLayer/TestAppLayer.cpp
@@ -1,5 +1,6 @@
 #include "TestAppLayer.hpp"
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/rotate_vector.hpp>
 
 float vertices[] = { // (x,y,z) (tex.x,tex.y) 36 vertices of 5 elements (sizeof(float))
 	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f, // 1
@@ -45,14 +46,22 @@ float vertices[] = { // (x,y,z) (tex.x,tex.y) 36 vertices of 5 elements (sizeof(
 	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 };
 
+struct VertexData
+{
+	glm::vec3 Position;
+	glm::vec2 TextureCoords;
+};
+
 void TestApp::OnAttach()
 {
     LOG_TRACE("TestAppLayer::OnAttach()");
     using namespace FE;
     using namespace RENDERER;
+	using namespace CORE;
 
     //register for events
     EventBus::AddListener<EVENTS::WindowResizeEvent&>("TestApp", BIND_EVENT_FN(OnWindowResizeEvent));
+	EventBus::AddListener<EVENTS::MouseScrollEvent&>("TestApp", BIND_EVENT_FN(OnMouseScrollEvent));
 
     // create the camera 
     Camera.Position = glm::vec3(0.0, 0.0f, 10.0f); // opengl right hand +Z is to me, -z is the background
@@ -60,20 +69,26 @@ void TestApp::OnAttach()
     Camera.FOV = 45.0f; // degrees 
     auto windowSize = RenderCommand::GetWindowSize();
     Camera.AspectRatio = windowSize.x / windowSize.y;
-	Camera.NearClip = 0.01f;
+	Camera.NearClip = 0.1f;
 	Camera.FarClip = 100.0f;
     Camera.Projection = glm::perspective(Camera.FOV,Camera.AspectRatio,Camera.NearClip,Camera.FarClip);
+	// camera movement stuff 
+	Camera.Front = glm::vec3{ 0.0f, 0.0f, -1.0f }; // unit vector defining the forward direction 
+	Camera.Up = glm::vec3{ 0.0f, 1.0f, 0.0f }; // unit vector defining the up direction;
+	Camera.View = glm::lookAt(Camera.Position, Camera.Position + Camera.Front, Camera.Up);
+	Camera.MouseSensitivity = 0.1f;
+	Camera.OldMouseValues = Input::GetMousePosition();
+	Camera.Pitch = 0.0f; // start with no pitch
+	Camera.Yaw = -90.0f; // cause openGL -Z 	
 
-    // cube stuff 
-	
-	
+    // cube stuff 	
 	Cube.VB = VertexBuffer::Create(36 * 5 * sizeof(float));
 	BufferLayout layout{
 		{"a_Position", BufferElementType::Float3},
 		{"a_TexCoord", BufferElementType::Float2}
 	};
 	Cube.VB->SetLayout(layout);
-	Cube.VB->SetData(vertices, 36 * 5 * sizeof(float));
+//	Cube.VB->SetData(vertices, 36 * 5 * sizeof(float));
 	
 	uint32_t indices[36];
 	for (auto i = 0; i < 36; i++)
@@ -128,7 +143,10 @@ void TestApp::OnAttach()
 	Cube.Texture2->Unbind();
 		
 	Cube.RotationDegree = 0.0f;
-	RenderCommand::SetFlag(ContextFlags::DepthTest, true);
+	RenderCommand::SetFlag(ContextRenderFlags::DepthTest, true);
+
+	// this should be in context;
+	RenderCommand::SetFlag(ContextWindowFlags::MouseCapture, true);
 }
 
 void TestApp::OnUpdate(FE::CORE::Timestep ts)
@@ -142,13 +160,57 @@ void TestApp::OnUpdate(FE::CORE::Timestep ts)
 	RenderCommand::ClearColor({0.1f,0.1f,0.15f,1.0f});
 	RenderCommand::Clear();
 	
-	// update camera
-	Camera.View = glm::translate(glm::mat4(1.0f), Camera.Position);
-	Camera.View = glm::inverse(Camera.View);
-	auto viewProjection = Camera.Projection* Camera.View;
+	// update camera movement
+	if (Input::Keypressed(KeyCode::W))
+	{
+		Camera.Position += Camera.Front * (Camera.TranslationSpeed * ts);
+	}
+	else if (Input::Keypressed(KeyCode::S))
+	{
+		Camera.Position -= Camera.Front * (Camera.TranslationSpeed * ts);
+	}
+	else if (Input::Keypressed(KeyCode::A))
+	{
+		Camera.Position -= glm::normalize(glm::cross(Camera.Front, Camera.Up)) * (Camera.TranslationSpeed * ts); // get the Direction (right) vector
+	}
+	else if (Input::Keypressed(KeyCode::D))
+	{
+		Camera.Position += glm::normalize(glm::cross(Camera.Front, Camera.Up)) * (Camera.TranslationSpeed * ts);
+	}
+		
+	// camera look around 
+	auto mousePos = Input::GetMousePosition();
 
+	//difference from old mouse position
+	glm::vec2 PosOffset{ mousePos.x - Camera.OldMouseValues.x,Camera.OldMouseValues.y - mousePos.y };
+
+	glm::vec2 deltaMovement = PosOffset * Camera.MouseSensitivity;
+	Camera.OldMouseValues = mousePos;
+
+	Camera.Yaw += deltaMovement.x;
+	Camera.Pitch += deltaMovement.y;
+
+	if (Camera.Pitch > 89.0f)
+		Camera.Pitch = 89.0f;
+	if (Camera.Pitch < -89.0f)
+		Camera.Pitch = -89.0f;
+
+	// calculate the direction the camera is facing 
+	glm::vec3 direction;
+	direction.x = cos(glm::radians(Camera.Yaw)) * cos(glm::radians(Camera.Pitch));
+	direction.y = sin(glm::radians(Camera.Pitch));
+	direction.z = sin(glm::radians(Camera.Yaw)) * cos(glm::radians(Camera.Pitch));
+	Camera.Front = glm::normalize(direction);
+
+	Camera.View = glm::lookAt(Camera.Position, Camera.Position + Camera.Front, Camera.Up);
 	
-	auto modelRotation = glm::rotate(glm::mat4(1.0f), glm::radians(Cube.RotationDegree), { 1.0f,0.5f,0.2f });
+	Camera.Projection = glm::perspective(glm::radians(Camera.FOV), Camera.AspectRatio, Camera.NearClip, Camera.FarClip);
+
+	auto viewProjection = Camera.Projection * Camera.View;
+		
+	//auto modelRotation = glm::rotate(glm::mat4(1.0f), glm::radians(Cube.RotationDegree), { 1.0f,0.0f,0.0f });
+	auto modelRotation = glm::rotate(glm::mat4(1.0f), glm::radians(0.0f), {0.0f,0.0f,1.0f});
+	
 	Cube.Texture->Bind(0);// bind slot 0
 	Cube.Texture2->Bind(1);// bind slot 0
 	Cube.TextureShader->Bind();
@@ -156,9 +218,29 @@ void TestApp::OnUpdate(FE::CORE::Timestep ts)
 	Cube.TextureShader->SetUniform("u_Texture2", 1); // look at slot 0 for texture
 	Cube.TextureShader->SetUniform("u_ViewProjection", viewProjection);
 	Cube.TextureShader->SetUniform("u_Model", modelRotation);
-	Cube.VAO->Bind();
-	RenderCommand::DrawIndexed(Cube.VAO, Cube.VAO->GetIndexCount());
-	Cube.VAO->Unbind();
+	for (int i = 0; i < 5; i++)
+	{
+		VertexData verts[36];
+		for (int j = 0; j < 36; j++)
+		{
+			uint32_t offset = j * 5;
+			
+			verts[j].Position.x = vertices[offset + 0] ;
+			verts[j].Position.y = vertices[offset + 1];
+			verts[j].Position.z = vertices[offset + 2];
+			verts[j].Position = glm::rotate(verts[j].Position, glm::radians(Cube.RotationDegree), {1.0f,1.0f,0.0f });
+			verts[j].Position += Positions[i];
+
+			verts[j].TextureCoords.x = vertices[offset + 3];
+			verts[j].TextureCoords.y = vertices[offset + 4];
+		}
+		// draw this cube 		
+		Cube.VB->SetData(verts, 36 * sizeof(VertexData));
+		
+		Cube.VAO->Bind();
+		RenderCommand::DrawIndexed(Cube.VAO, Cube.VAO->GetIndexCount());
+		Cube.VAO->Unbind();
+	}
 	Cube.TextureShader->Unbind();	
 }
 
@@ -171,4 +253,15 @@ void TestApp::OnWindowResizeEvent(FE::EVENTS::WindowResizeEvent& event)
 {
     Camera.AspectRatio = event.GetWidth() / static_cast<float>(event.GetHeight());
 	Camera.Projection = glm::perspective(Camera.FOV, Camera.AspectRatio, Camera.NearClip, Camera.FarClip);
+}
+
+void TestApp::OnMouseScrollEvent(FE::EVENTS::MouseScrollEvent& event)
+{
+	auto offset = event.GetYOffset();
+	Camera.FOV -= event.GetYOffset();
+
+	if (Camera.FOV < 1.0f)
+		Camera.FOV = 1.0f;
+	if (Camera.FOV > 45.0f)
+		Camera.FOV = 45.0f;
 }
--- TestApp3D/src/TestAppLayer/TestAppLayer.hpp
+++ TestApp3D/src/TestAppLayer/TestAppLayer.hpp
@@ -13,19 +13,37 @@ public:
     void OnDetach() override;
 
     void OnWindowResizeEvent(FE::EVENTS::WindowResizeEvent& event);
+    void OnMouseScrollEvent(FE::EVENTS::MouseScrollEvent& event);
 private:
 
 private:
     struct CameraData
     {
+        float TranslationSpeed{ 10.0f };
         float FOV, NearClip, FarClip, AspectRatio;
         glm::mat4 Projection;
         glm::mat4 View; // aka, camera transform
-        glm::vec3 Position;        
-    } Camera;
 
+
+        glm::vec3 Position;
+        glm::vec3 Front;
+        glm::vec3 Up;
+
+        glm::vec2 OldMouseValues;
+        float MouseSensitivity;
+        float Yaw, Pitch;        
+
+    } Camera;
+    glm::vec3 Positions[5] =
+    {
+        { 0.0f, 0.0f, 0.0f},
+        { 5.0f, 0.0f, 0.0f},
+        {-5.0f, 0.0f, 0.0f},
+        { 0.0f, 5.0f, 0.0f},
+        { 0.0f,-5.0f, 0.0f}
+    };
     struct CubeData
-    {        
+    {
         float RotationDegree ;
         Ref<FE::RENDERER::VertexArray> VAO;
         Ref<FE::RENDERER::IndexBuffer> IB;
